215ce15d8cb7fc5c268a2d4715cd85d8d2bee544
gusscamargo <guga.power@hotmail.com>
2021-12-02 20:24:30 -0300

    Gambiarra pra passar por cima desse bug sem sentido, codagem não ta como eu quero que seja

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,16 @@ const router = require('express').Router()
 const indexController = require("../controllers/indexController")
 
 /* GET home page. */
-router.get("*", indexController.index)
+// Como deveria ser:
+// router.get("*", indexController.index)
+
+// Por algum motivo ta tendo bug na relação route-controller, bug que não ta faezendo sentido
+router.get("*", (req, res, next) => {
+  res.render('index', { 
+    title: 'Descobrir seu UserAgent e IP',
+    userAgent: req.get("User-Agent"),
+    userIp: req.ip
+  });
+})
 
 module.exports = router
